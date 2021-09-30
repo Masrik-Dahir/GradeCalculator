8cdc35049687b34ba64dbbeba7728e70ce5e269c
Masrik Dahir <dahirma@vcu.edu>
2021-09-29 23:08:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ if __name__ == '__main__':
     
     dic_303 = {"Homework":  [.1,[1,1]],
            "Classwork": [.1,[1,1]],
-           "Quiz_1":  [.1, [.796875]],
+           "Quiz_1":  [.1, [0.895]],
            "Quiz_2": [.1, [1]],
            "Quiz_3": [.1,[1]],
            "Mid_term": [.25, [.9]],
            "Final":[.25,[.9]]
            }
 
-    dic_401 = {"Programming_Assignment": [.4, [0.65, 1, 1, 1]], #4 Assignments
+    dic_401 = {"Programming_Assignment": [.4, [0.75, 1, 1, 1]], #4 Assignments
                "Theory_Assignment": [.1, [.8, 1]], #2 Assignments
                "Test_1": [.15, [.88]],
                "Test_2": [.15, [.80]],
